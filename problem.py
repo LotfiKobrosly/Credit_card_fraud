9dcd7ec0ec37d3cfc0a745a67c5eae7a96538c19
Yuhe BAI <baiyuhe1314@gmail.com>
2021-01-13 23:44:52 +0100

    commit

--- problem.py
+++ problem.py
@@ -1,8 +1,43 @@
 import os
 
-import numpy as np
 import pandas as pd
 import rampwf as rw
-from rampwf.score_types.base import BaseScoreType
 from sklearn.model_selection import ShuffleSplit
-from sklearn.metrics import mean_squared_error
+
+problem_title = 'Credit Card Fraud Detection'
+_target_column_name = 'type'
+_ignore_column_names = []
+_prediction_label_names = [1.0, 2.0]
+
+# A type (class) which will be used to create wrapper objects for y_pred
+Predictions = rw.prediction_types.make_multiclass(
+    label_names=_prediction_label_names)
+
+# An object implementing the workflow
+workflow = rw.workflows.Estimator()
+
+score_types = [
+    rw.score_types.Accuracy(name='acc', precision=5),
+]
+
+
+def get_cv(X, y):
+    cv = ShuffleSplit(n_splits=8, test_size=0.20, random_state=57)
+    return cv.split(X, y)
+
+
+def _read_data(path, f_name):
+    data = pd.read_csv(os.path.join(path, 'data', f_name))
+    y_array = data[_target_column_name].values
+    X_df = data.drop(_target_column_name, axis=1)
+    return X_df, y_array
+
+
+def get_train_data(path='.'):
+    f_name = 'train.csv'
+    return _read_data(path, f_name)
+
+
+def get_test_data(path='.'):
+    f_name = 'test.csv'
+    return _read_data(path, f_name)
---
+++ split_data.py
@@ -0,0 +1,39 @@
+import csv
+import os
+import numpy as np
+
+'''Divide the data in creditcard.csv into two csv files train.csv and test.csv Among them, there are 227846 data in train.csv and 56961 data in test.csv'''
+labels = []
+data = []
+a_train_file = 'train.csv'
+a_test_file = 'test.csv'
+a_file = 'creditcard.csv'
+
+seed = 3
+np.random.seed(seed)
+train_indices = np.random.choice(284807, 227846, replace=False) # Set random number generation to randomly select 80% random numbers
+residue = np.array(list(set(range(284807)) - set(train_indices)))
+# If the combined data of the training set and the test set add up to a whole data set, this operation is not required
+test_indices = np.random.choice(len(residue), 56961, replace=False) 
+with open(a_file)as afile:
+    a_reader = csv.reader(afile)  # Read all data from the original data set and save it to a_reader
+    labels = next(a_reader)  # Extract the first row and set it as labels
+    for row in a_reader:  # Extract the data of each row in a_reader and save it in the data list
+        data.append(row)
+
+
+# generate train.csv
+if not os.path.exists(a_train_file):
+    with open(a_train_file, "w", newline='') as a_trian:
+        writer = csv.writer(a_trian)
+        writer.writerows([labels])  
+        writer.writerows(np.array(data)[train_indices])
+        a_trian.close()
+
+# generate test.csv
+if not os.path.exists(a_test_file):
+    with open(a_test_file, "w", newline='')as a_test:
+        writer = csv.writer(a_test)
+        writer.writerows([labels])  
+        writer.writerows(np.array(data)[test_indices])
+        a_test.close()
